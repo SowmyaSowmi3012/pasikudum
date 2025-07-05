9ce15b33f2c200d34d8188a918fede1f08b4062a
SowmyaSowmi3012 <sowmisowmya402@gmail.com>
2025-07-05 17:42:13 +0530

    Update firebase admin config and add .env.example

--- backend/src/firebase/firebaseAdmin.js
+++ backend/src/firebase/firebaseAdmin.js
@@ -1,7 +1,12 @@
 const admin = require("firebase-admin");
 const path = require("path");
+require("dotenv").config();
 
-const serviceAccount = require(path.join(__dirname, "pasikudu.json"));
+// Set the path to the credentials if not set already
+process.env.GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS || './secure/pasikudu.json';
+
+// Load the service account key
+const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 if (!admin.apps.length) {
   admin.initializeApp({
